ed6a1f94b221a6ab2fc520da68ec3860c0e3c39b
notbraydon <braydonwestmoreland@gmail.com>
2018-10-26 15:30:50 -0500

    Update master_state_machine.v

--- master_state_machine.v
+++ master_state_machine.v
@@ -10,30 +10,31 @@
 
 module mealy(
      input clk,
-     input [3:0] color, // wire [4:0] - color needs to be 3 bits
-     input IR,
+     input [2:0] color,
+     input IRS,
      input IPS,
      output reg out, // IDK what this is yet...
      output reg enable_searching = 0
      );
      
-     wire [4:0] inputs; // input bus
-     assign inputs = {IR, IPS, color[3], color[2], color[1]};
+     wire [4:0] inputs; // bus to combine all 3 input sources
+     assign inputs = {IRS, IPS, color[2], color[1], color[0]};
+     
      // Define states as parameters
      parameter S_Search = 0,    // Search field for washer.     Need to use: IRS, H-bridge (& pwm module), IPS, servo(s).
-               S_Avoid = 1,
+               S_Avoid = 1,     // Avoid obstacle.              Need to use:
                S_Grab = 2,      // Pick the washer up.          Need to use: E-mag, servo(s).
                S_Color = 3,     // Determine washer color.      Need to use: color sensor, maybe backlight. (Scan multiple times - if no color is found washer is dropped and needs to go back to S_Search)
                S_Corner = 4,    // Find correct corner LED.     Need to use: IRS, H-bridge (& pwm module), phototransistor (need to figure out mounting and if it needs a servo).
-               S_Drop = 5;      // Drop the washer off.         Need to use: Servo(s), E-mag?
-               // May need states to reposition washer before picking up and dropping off the washer.
+               S_Drop = 5,      // Drop the washer off.         Need to use: Servo(s), E-mag?
+               S_Stalled = 6;   // Motors have stalled.         Need to use:
 
 
      // Create a register for the current state, initialize it to S_Search
      reg state_now = S_Search;
      // Create a register for the next state
      reg state_next;
-
+     
 
      // Process 1: Determine next state and output
      always @ (state_now)
@@ -58,67 +59,68 @@ module mealy(
                     endcase
                
                S_Avoid:
-                    case(inputs)
-                    0: begin
+                    casex(inputs) // needs a previous_state input to know where to return to
+                    5'b10xxx: begin
                          state_next <= S_Avoid;
-                         out = 0;
                     end
-                    1: begin
+                    5'b00xxx: begin // needs prev_state
                          state_next <= S_Search;
-                         out = 0;
                     end
-                    1: begin
+                    5'b00xxx: begin // needs prev_state
                          state_next <= S_Corner;
-                         out = 0;
                     end  
                     endcase
                     
                S_Grab:
-                    case(inputs)
-                    0: begin
+                    casex(inputs)
+                    5'b01xxx: begin // IPS still detects washer
                          state_next <= S_Grab;
-                         out = 0;
                     end
-                    1: begin
+                    5'b00xxx: begin
                          state_next <= S_Color;
-                         out = 0;
                     end
                     endcase
                
                S_Color:
-                    case(inputs)
-                    0: begin
+                    casex(inputs)
+                    5'b00xxx: begin
                          state_next <= S_Color;
-                         out = 0;
                     end
-                    1: begin
+                    5'b00xxx: begin
                          state_next <= S_Corner;
-                         out = 0;
                     end
                     endcase   
                     
                S_Corner:
-                    case(inputs)
-                    0: begin
+                    casex(inputs)
+                    5'b00xxx: begin
                          state_next <= S_Corner;
-                         out = 0;
                     end
-                    1: begin
+                    5'b00xxx: begin
                          state_next <= S_Drop;
-                         out = 0;
                     end
                     endcase
                          
                S_Drop:
-                    case(inputs)
-                    0: begin
+                    casex(inputs)
+                    5'b00xxx: begin
                          state_next <= S_Drop;
-                         out = 0;
                     end
-                    1: begin
+                    5'b00xxx: begin
                          state_next <= S_Search;
-                         out = 0; 
                     end
+                    endcase
+               
+               S_Stalled:
+                    casex(inputs) // needs a previous_state input to know where to return to
+                    5'b00xxx: begin
+                         state_next <= S_Stalled;
+                    end
+                    5'b00xxx: begin
+                         state_next <= S_Search;
+                    end
+                    5'b00xxx: begin
+                         state_next <= S_Corner;
                     endcase
           endcase
 
@@ -127,98 +129,4 @@ module mealy(
      always @ (posedge clk)
           state_now <= state_next;
 
-endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////
-// OLD VERSION USING MOORE
-//////////////////////////////////////////////////////////////////////////////////
-
-// Moore machine; output only depends on current state
-module moore(
-     input clk,
-     input in,
-     output reg out
-     );
-
-     // Define states as parameters
-     parameter S0 = 000, // Searching
-               S1 = 001, // Pick up
-               S2 = 010, // Find color
-               S3 = 011, // Find corner
-               S4 = 100; // Drop off
-
-     // Create a register for the current state, initialize it to S0
-     reg state_now = S0;
-     // Create a register for the next state
-     reg state_next;
-
-     // Determine next state
-     always @ (state_now)
-          case(state_now)
-               S0:
-                    case(in)
-                    0: state_next <= S0;
-                    1: state_next <= S1;
-                    endcase
-               S1:
-                    case(in)
-                    0: state_next <= S1;
-                    1: state_next <= S2;
-                    endcase
-               S2:
-                    case(in)
-                    0: state_next <= S2;
-                    1: state_next <= S3;
-                    endcase
-               S3:
-                    case(in)
-                    0: state_next <= S3;
-                    1: state_next <= S4;
-                    endcase 
-               S4:
-                    case(in)
-                    0: state_next <= S4;
-                    1: state_next <= S0;
-                    endcase                   
-          endcase
-
-     // Move next state into current state
-     always @ (posedge clk)
-          state_now <= state_next;
-
-     // Output according to current state
-     always @ (state_now)
-          case(state_now)
-          S0: out = 000;
-          S1: out = 001;
-          S2: out = 010;
-          S3: out = 011;
-          S4: out = 100;
-          endcase
 endmodule
