192b4fbecc57c52e0a19639be6da062c6c10a597
notbraydon <braydonwestmoreland@gmail.com>
2018-10-26 14:32:28 -0500

    Update master_state_machine.v

--- master_state_machine.v
+++ master_state_machine.v
@@ -10,16 +10,22 @@
 
 module mealy(
      input clk,
-     input in, // I think this comes from the other modules as an input back into the state machine??
-     output reg out // IDK what this is yet...
+     input [3:0] color, // wire [4:0] - color needs to be 3 bits
+     input IR,
+     input IPS,
+     output reg out, // IDK what this is yet...
+     output reg enable_searching = 0
      );
-
+     
+     wire [4:0] inputs; // input bus
+     assign inputs = {IR, IPS, color[3], color[2], color[1]};
      // Define states as parameters
      parameter S_Search = 0,    // Search field for washer.     Need to use: IRS, H-bridge (& pwm module), IPS, servo(s).
-               S_Grab = 1,      // Pick the washer up.          Need to use: E-mag, servo(s).
-               S_Color = 2,     // Determine washer color.      Need to use: color sensor, maybe backlight. (Scan multiple times - if no color is found washer is dropped and needs to go back to S_Search)
-               S_Corner = 3,    // Find correct corner LED.     Need to use: IRS, H-bridge (& pwm module), phototransistor (need to figure out mounting and if it needs a servo).
-               S_Drop = 4;      // Drop the washer off.         Need to use: Servo(s), E-mag?
+               S_Avoid = 1,
+               S_Grab = 2,      // Pick the washer up.          Need to use: E-mag, servo(s).
+               S_Color = 3,     // Determine washer color.      Need to use: color sensor, maybe backlight. (Scan multiple times - if no color is found washer is dropped and needs to go back to S_Search)
+               S_Corner = 4,    // Find correct corner LED.     Need to use: IRS, H-bridge (& pwm module), phototransistor (need to figure out mounting and if it needs a servo).
+               S_Drop = 5;      // Drop the washer off.         Need to use: Servo(s), E-mag?
                // May need states to reposition washer before picking up and dropping off the washer.
 
 
@@ -33,19 +39,42 @@ module mealy(
      always @ (state_now)
           case(state_now)
                S_Search:
-                    case(in)
-                    0: begin // no washer found, still searching field
+                    casex(inputs) // dont care about color sensor bits
+                    5'b00xxx: begin // no washer found, no object detected, still searching field
                          state_next <= S_Search;
-                         out = 0; // ????? becomes 'in' ??? IDK
+                         enable_searching = 1; // turns on seperate searching module
+                         // have this state output enable bit that tells movement module 
+                         // multiplexer needed??
+                    end
+                    5'b10xxx: begin // IRS object detected
+                         state_next <= S_Avoid;
+                         enable_searching = 0; // ??
+                         
+                    end
+                    5'b01xxx: begin // IPS washer detected
+                         state_next <= S_Grab;
+                         enable_searching = 0;
+                    end
+                    endcase
+               
+               S_Avoid:
+                    case(inputs)
+                    0: begin
+                         state_next <= S_Avoid;
+                         out = 0;
                     end
                     1: begin
-                         state_next <= S_Grab;
-                         out = 1;
+                         state_next <= S_Search;
+                         out = 0;
                     end
+                    1: begin
+                         state_next <= S_Corner;
+                         out = 0;
+                    end  
                     endcase
                     
                S_Grab:
-                    case(in)
+                    case(inputs)
                     0: begin
                          state_next <= S_Grab;
                          out = 0;
@@ -57,7 +86,7 @@ module mealy(
                     endcase
                
                S_Color:
-                    case(in)
+                    case(inputs)
                     0: begin
                          state_next <= S_Color;
                          out = 0;
@@ -69,7 +98,7 @@ module mealy(
                     endcase   
                     
                S_Corner:
-                    case(in)
+                    case(inputs)
                     0: begin
                          state_next <= S_Corner;
                          out = 0;
@@ -81,7 +110,7 @@ module mealy(
                     endcase
                          
                S_Drop:
-                    case(in)
+                    case(inputs)
                     0: begin
                          state_next <= S_Drop;
                          out = 0;
