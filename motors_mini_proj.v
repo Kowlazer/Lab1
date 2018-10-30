dbc3dcdacf72d69c4d9c4be7d2b96a141c540550
notbraydon <braydonwestmoreland@gmail.com>
2018-10-30 16:47:00 -0500

    Update motors_mini_proj.v

--- motors_mini_proj.v
+++ motors_mini_proj.v
@@ -6,7 +6,7 @@
 `timescale 1ns / 1ps
 
 module motors_mini_proj(
-    input clk,
+    input clock,
     input [7:0] sw,
     output LeftPWMOut,
     output RightPWMOut,
@@ -16,19 +16,29 @@ module motors_mini_proj(
     wire [5:0] speed; // wire is used at any block asigned using an assign statement using continuous assignment
     assign speed = sw[5:0];
     
+    // IDK if the following three lines are needed > added when debugging > try removing
+    wire pwm_L, pwm_R;
+    assign LeftPWMOut = pwm_L;
+    assign RightPWMOut = pwm_R;
+    
     // module instantiation
-    motors_pwm(
+    motors_pwm L(
+        .clk(clock),
         .duty(speed),
-        .clk(clk),
-        .PWM_output(LeftPWMOut),
-        .PWM_output(RightPWMOut) // made motors use same PWM; may change later; if not make Left/RightPWMOut the same (in .xdc)
+        .PWM_output(pwm_L)
     );
+    motors_pwm R(
+            .clk(clock),
+            .duty(speed),
+            .PWM_output(pwm_R)
+        );
 
-    always@(posedge clk)
+
+    always@(posedge clock)
         begin
             case(sw[7:6])
-                1'b0: TankDir <= 4'b1001; // forward
-                1'b1: TankDir <= 4'b0110; // reverse
+                2'b00: TankDir <= 4'b0110; // forward
+                2'b11: TankDir <= 4'b1001; // reverse
                 2'b10: TankDir <= 4'b1010; // left
                 2'b01: TankDir <= 4'b0101; // right
                 default: TankDir <= 4'b0000; // off
