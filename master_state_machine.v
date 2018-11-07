973342953724fb5aed3e5acb08d013d3fb111f96
notbraydon <braydonwestmoreland@gmail.com>
2018-11-07 11:22:23 -0600

    Update master_state_machine.v

--- master_state_machine.v
+++ master_state_machine.v
@@ -27,6 +27,7 @@ module top(
      reg state_now = S_Search;
      reg state_next = S_Search;
      
+     
      // Process 1: Determine next state and output (no outputs right now)
      always @ (state_now)
           case(state_now)
@@ -37,38 +38,45 @@ module top(
                          state_next <= S_Search;
                          enable_search = 1; // turns on seperate searching module
                     end
-                    8'b10_00_0_xxx: begin // Left IRS triggered
+                    8'b10_00_0_xxx: begin // Left IRS
                          state_next <= S_Avoid;
                     end
-                    8'b01_00_0_xxx: begin // Right IRS triggered
+                    8'b01_00_0_xxx: begin // Right IRS
                          state_next <= S_Avoid;
                     end
-                    8'b11_00_0_xxx: begin // Both IRS triggered (RARE)
+                    8'b11_00_0_xxx: begin // Both IRS (RARE)
                          state_next <= S_Avoid;
                     end
-                    8'bxx_1x_x_xxx: begin // Sense_A triggered
+                    8'bxx_1x_x_xxx: begin // Sense A
                          state_next <= S_Stall;
                     end
-                    8'bxx_x1_x_xxx: begin // Sense_B triggered
+                    8'bxx_x1_x_xxx: begin // Sense B
                          state_next <= S_Stall;
                     end
-                    8'bxx_xx_1_xxx: begin // IPS triggered
+                    8'bxx_xx_1_xxx: begin // IPS
                          state_next <= S_Grab;
                     end
                     endcase
 
                S_Avoid:
-                    casex(inputs) // needs a previous_state input to know where to return to
-                    8'bxx_xx_x_xxx: begin // IRS still detects object
+                    // not 100% how this state will work yet; might be a "go ... done" system instead
+                    casex(inputs)
+                    8'b10_xx_x_xxx: begin // Left IRS still
                          state_next <= S_Avoid;
                     end
-                    8'bxx_xx_x_xxx: begin // Path is clear
+                    8'b01_xx_x_xxx: begin // Right IRS still
+                         state_next <= S_Avoid;
+                    end
+                    8'b11_xx_x_xxx: begin // Both IRS still (RARE)
+                         state_next <= S_Avoid;
+                    end
+                    8'b00_xx_x_xxx: begin // Path is clear
                          state_next <= state_previous;
                     end
                     endcase   
           endcase
 
-
+     
      // Process 2: Move next state into current state
      always @ (posedge clk)
           state_now <= state_next;
